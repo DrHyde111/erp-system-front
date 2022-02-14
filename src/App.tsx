c7862c9fd5551e3b83c1ae8f1e70a4897dfb3e30
DrHyde111 <pawel.j.skalski@gmail.com>
2022-02-14 12:42:16 +0100

    Set up react-bootstrap, bootstrap-icons, react-router

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import {Navbar} from "react-bootstrap";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App-header bg-light">
+            <div className="App container">
+                <Navbar collapseOnSelect expand="md" className="mb-3">
+                    <Navbar.Brand className="font-weight-bold text-muted">
+                        ERP-System
+                    </Navbar.Brand>
+                </Navbar>
+            </div>
+        </div>
+    );
 }
 
 export default App;
