b6b73841ffba12207f39332b2cc3b58b42003459
DrHyde111 <pawel.j.skalski@gmail.com>
2022-04-29 18:08:57 +0200

    Changed some routes to be admin only

--- src/PrivateSwitch.tsx
+++ src/PrivateSwitch.tsx
@@ -21,8 +21,11 @@ import WarehouseProducts from "./components/products/WarehouseProducts";
 import AddProduct from "./components/products/AddProduct";
 import Product from "./components/products/Product";
 import MoveProduct from "./components/products/MoveProduct";
+import {useAppContext} from "./services/context.services";
 
 export default function PrivateSwitch() {
+    const context = useAppContext()
+    const userRole = context.user.Role
     return (
         <Routes>
             <Route path="*" element={<NotFound/>}/>
@@ -31,21 +34,28 @@ export default function PrivateSwitch() {
             <Route path="/time-register/attendances" element={<AllEmployeeAttendnace/>}/>
             <Route path="/time-register/attendances/:attendanceId/remarks" element={<SpecificAttendanceRemarks/>}/>
             <Route path="/time-register/attendances/:attendanceId/remarks/:remarkId" element={<Remark/>}/>
-            <Route path="/employees/:id/edit" element={<EditEmployee/>}/>
-            <Route path="/employees/:id/attendance" element={<EmployeeAttendances/>}/>
-            <Route path="/employees/:id/attendance/:attendanceId/remarks" element={<SpecificAttendanceRemarks/>}/>
-            <Route path="/employees/:id/attendance/:attendanceId/remarks/:remarkId" element={<Remark/>}/>
-            <Route path="/employees/:id" element={<Employee/>}/>
-            <Route path="/employees/create" element={<CreateEmployee/>}/>
-            <Route path="/employees" element={<AllEmployees/>}/>
-            <Route path="/warehouses/:id/overseers/assign" element={<AssignOverseer/>}/>
-            <Route path="/warehouses/:id/overseers" element={<WarehouseOverseers/>}/>
+            {userRole == "Admin" ? (
+                <>
+                    <Route path="/employees/:id/edit" element={<EditEmployee/>}/>
+                    <Route path="/employees/:id/attendance" element={<EmployeeAttendances/>}/>
+                    <Route path="/employees/:id/attendance/:attendanceId/remarks"
+                           element={<SpecificAttendanceRemarks/>}/>
+                    <Route path="/employees/:id/attendance/:attendanceId/remarks/:remarkId" element={<Remark/>}/>
+                    <Route path="/employees/:id" element={<Employee/>}/>
+                    <Route path="/employees/create" element={<CreateEmployee/>}/>
+                    <Route path="/employees" element={<AllEmployees/>}/>
+                    <Route path="/warehouses/:id/overseers/assign" element={<AssignOverseer/>}/>
+                    <Route path="/warehouses/:id/overseers" element={<WarehouseOverseers/>}/>
+                    <Route path="/warehouses/create" element={<CreateWarehouse/>}/>
+                </>
+            ) : (
+                <></>
+            )}
             <Route path="/warehouses/:id/products/:productId/move" element={<MoveProduct/>}/>
             <Route path="/warehouses/:id/products/:productId" element={<Product/>}/>
             <Route path="/warehouses/:id/products/add" element={<AddProduct/>}/>
             <Route path="/warehouses/:id/products" element={<WarehouseProducts/>}/>
             <Route path="/warehouses/:id" element={<Warehouse/>}/>
-            <Route path="/warehouses/create" element={<CreateWarehouse/>}/>
             <Route path="/warehouses" element={<AllWarehouses/>}/>
         </Routes>
     );
